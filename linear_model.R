7e8d317efb3a8ea009575ec36529ab073d52e996
Enterprise-D <jiac.s@outlook.com>
2023-11-24 17:16:27 -0500

    in construction

--- linear_model.R
+++ linear_model.R
@@ -1,30 +1,30 @@
 library(dplyr)
 library(stringi)
 
-glm_train = function(chrv, startv, pos, neg) {
-  pos_cnv = read.table(paste0("data/lm_summarized/lm_train_", pos, "/", pos, "_cnv.txt"), header = T) %>%
+prepare_dataset = function(chrv, startv, pos, neg) {
+  pos_cnv = read.table(paste0("data/lm_summarized/lm_", pos, "/", pos, "_cnv.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  pos_ratio = read.table(paste0("data/lm_summarized/lm_train_", pos, "/", pos, "_ratio.txt"), header = T) %>%
+  pos_ratio = read.table(paste0("data/lm_summarized/lm_", pos, "/", pos, "_ratio.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  pos_gini = read.table(paste0("data/lm_summarized/lm_train_", pos, "/", pos, "_gini.txt"), sep = "\t", header = T) %>%
+  pos_gini = read.table(paste0("data/lm_summarized/lm_", pos, "/", pos, "_gini.txt"), sep = "\t", header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
   pos_label = as.data.frame(rep(1, nrow(pos_cnv)))
 
-  neg_cnv = read.table(paste0("data/lm_summarized/lm_train_", neg, "/", neg, "_cnv.txt"), header = T) %>%
+  neg_cnv = read.table(paste0("data/lm_summarized/lm_", neg, "/", neg, "_cnv.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  neg_ratio = read.table(paste0("data/lm_summarized/lm_train_", neg, "/", neg, "_ratio.txt"), header = T) %>%
+  neg_ratio = read.table(paste0("data/lm_summarized/lm_", neg, "/", neg, "_ratio.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  neg_gini = read.table(paste0("data/lm_summarized/lm_train_", neg, "/", neg, "_gini.txt"), sep = "\t", header = T) %>%
+  neg_gini = read.table(paste0("data/lm_summarized/lm_", neg, "/", neg, "_gini.txt"), sep = "\t", header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
@@ -38,45 +38,33 @@ glm_train = function(chrv, startv, pos, neg) {
 
   training_set = rbind(pos_set, neg_set)
 
-  remove(pos_cnv, pos_ratio, pos_gini, pos_label, neg_cnv, neg_ratio, neg_gini, neg_label, pos_set, neg_set)
-
   training_set = training_set[training_set$cnv <= 100,]
-
-  linear_model = glm(label ~ cnv + ratio + gini, data = training_set, family = 'binomial')
-
-  summary(linear_model)
-
-  coef = summary(linear_model)$coef
-  write.table(coef, file = 'lm_coef_model.txt', row.names = T, col.names = T, sep = '\t', quote = F)
-
-  return(linear_model)
 }
 
-glm_validate = function(linear_model, chrv, startv, pos, neg) {
-
-  pos_cnv = read.table(paste0("data/lm_summarized/lm_val_", pos, "/", pos, "_cnv.txt"), header = T) %>%
+prepare_dataset_temp = function(chrv, startv, pos, neg) {
+  pos_cnv = read.table(paste0("data/lm_summarized/lm_", pos, "_cnv.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  pos_ratio = read.table(paste0("data/lm_summarized/lm_val_", pos, "/", pos, "_ratio.txt"), header = T) %>%
+  pos_ratio = read.table(paste0("data/lm_summarized/lm_", pos, "_ratio.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  pos_gini = read.table(paste0("data/lm_summarized/lm_val_", pos, "/", pos, "_gini.txt"), sep = "\t", header = T) %>%
+  pos_gini = read.table(paste0("data/lm_summarized/lm_", pos, "_gini.txt"), sep = "\t", header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
   pos_label = as.data.frame(rep(1, nrow(pos_cnv)))
 
-  neg_cnv = read.table(paste0("data/lm_summarized/lm_val_", neg, "/", neg, "_cnv.txt"), header = T) %>%
+  neg_cnv = read.table(paste0("data/lm_summarized/lm_", neg, "_cnv.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  neg_ratio = read.table(paste0("data/lm_summarized/lm_val_", neg, "/", neg, "_ratio.txt"), header = T) %>%
+  neg_ratio = read.table(paste0("data/lm_summarized/lm_", neg, "_ratio.txt"), header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
-  neg_gini = read.table(paste0("data/lm_summarized/lm_val_", neg, "/", neg, "_gini.txt"), sep = "\t", header = T) %>%
+  neg_gini = read.table(paste0("data/lm_summarized/lm_", neg, "_gini.txt"), sep = "\t", header = T) %>%
     filter(chr == chrv & start == startv) %>%
     select(-chr, -start, -end) %>%
     t()
@@ -88,24 +76,51 @@ glm_validate = function(linear_model, chrv, startv, pos, neg) {
   colnames(pos_set) = c("cnv", "ratio", "gini", "label")
   colnames(neg_set) = c("cnv", "ratio", "gini", "label")
 
-  validation_set = rbind(pos_set, neg_set)
+  training_set = rbind(pos_set, neg_set)
 
-  remove(pos_cnv, pos_ratio, pos_gini, pos_label, neg_cnv, neg_ratio, neg_gini, neg_label, pos_set, neg_set)
-
-  validation_set[validation_set$cnv > 100, "cnv"] = 100
-
-  validation_set$pred = predict(linear_model, validation_set, type = "response")
-  validation_set$pred[is.na(validation_set$pred)] = 0
-
-  ident = paste0(stri_replace(pos, fixed = "LC", replacement = ""), stri_replace(neg, fixed = "LC", replacement = ""))
-
-  write.table(validation_set, file = paste0('data/lm_summarized/prediction_', ident, '.csv'), row.names = F, col.names = F, sep = ",", quote = F)
+  training_set = training_set[training_set$cnv <= 100,]
 }
 
-linear_model = glm_train(chrv = "chr7", startv = 55000000, pos = "LC499", neg = "LC500")
+#dataset_1 = prepare_dataset(chrv = "chr7", startv = 55000000, pos = "LC499", neg = "LC500")
+dataset_2 = prepare_dataset(chrv = "chr8", startv = 127000000, pos = "LC676", neg = "LC677")
 
-glm_validate(linear_model, chrv = "chr7", startv = 55000000, pos = "LC499", neg = "LC500")
+training_set_1 = prepare_dataset_temp(chrv = "chr7", startv = 55000000, pos = "train_LC499/LC499", neg = "train_LC500/LC500")
+validation_set_1 = prepare_dataset_temp(chrv = "chr7", startv = 55000000, pos = "val_LC499/LC499", neg = "val_LC500/LC500")
 
-glm_validate(linear_model, chrv = "chr8", startv = 127000000, pos = "LC729", neg = "LC730")
+#val_barcode_1 = read.table("data/processed/barcode_val_499500.txt", header = F)$V1
+#training_set_1 = dataset_1[!rownames(dataset_1) %in% val_barcode_1,]
+#validation_set_1 = dataset_1[rownames(dataset_1) %in% val_barcode_1,]
 
-glm_validate(linear_model, chrv = "chr8", startv = 127000000, pos = "LC676", neg = "LC677")
+val_barcode_2 = read.table("data/processed/barcode_val_676677.txt", header = F)$V1
+training_set_2 = dataset_2[!rownames(dataset_2) %in% val_barcode_2,]
+validation_set_2 = dataset_2[rownames(dataset_2) %in% val_barcode_2,]
+
+training_set = rbind(training_set_1, training_set_2)
+validation_set = rbind(validation_set_1, validation_set_2)
+
+linear_model = glm(label ~ cnv + ratio + gini, data = training_set_1, family = 'binomial')
+
+summary(linear_model)
+
+coef = summary(linear_model)$coef
+write.table(coef, file = 'lm_coef_model_676677.txt', row.names = T, col.names = T, sep = '\t', quote = F)
+
+######## validation ########
+
+validation_set[validation_set$cnv > 100, "cnv"] = 100
+
+validation_set$pred = predict(linear_model, validation_set, type = "response")
+validation_set$pred[is.na(validation_set$pred)] = 0
+
+write.table(validation_set, file = "data/lm_summarized/prediction_all_676677.csv", row.names = F, col.names = F, sep = ",", quote = F)
+
+######### test #########
+dataset_3 = prepare_dataset(chrv = "chr8", startv = 127000000, pos = "LC729", neg = "LC730")
+
+dataset_3[dataset_3$cnv > 100, "cnv"] = 100
+
+
+dataset_3$pred = predict(linear_model, dataset_3, type = "response")
+dataset_3$pred[is.na(dataset_3$pred)] = 0
+
+write.table(dataset_3, file = "data/lm_summarized/prediction_729730_mixed.csv", row.names = F, col.names = F, sep = ",", quote = F)
