20281eab4f1dd64e6e3a70748a88210cb21bfe41
Enterprise-D <jiac.s@outlook.com>
2023-12-14 20:11:01 -0500

    finalized model.

--- CMplot.R
+++ CMplot.R
@@ -1,6 +1,6 @@
 library("CMplot")
 
-count_freq_file = "data/assessment/LC675_count_freq.txt"
+count_freq_file = "ecDNA_summary_lm_val_499500_0.50.txt"
 
 a <- read.table(count_freq_file, header = T)
 for (i in seq_len(nrow(a))) {
@@ -19,5 +19,5 @@ print(a[a$freq > 0.01,])
 CMplot(b, plot.type = "m", col = c("grey30", "grey60"), LOG10 = FALSE, ylim = c(0, 1), threshold = c(0.05, 0.1),
        threshold.lty = c(2, 1), threshold.lwd = c(1, 1), threshold.col = c("black", "black"), amplify = TRUE,
        chr.den.col = NULL, signal.col = c("red", "orange"), signal.cex = c(1.5, 1.5), signal.pch = c(19, 19),
-       main = "LC675_linear_model", ylab = 'Proportion', axis.cex = 0.7,
-       file = "jpg", file.name = "LC675_linear_model", dpi = 300, file.output = TRUE, verbose = TRUE, width = 14, height = 6)
+       main = "cutoff=0.50", ylab = 'Proportion', axis.cex = 0.7,
+       file = "jpg", file.name = "0.50", dpi = 300, file.output = TRUE, verbose = TRUE, width = 14, height = 6)
